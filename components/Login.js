183cf4cb871d18348ace544a7a2901a705c78ad9
Mathieu <mathieunr@hotmail.fr>
2024-02-16 10:15:59 +0100

    ajou composant signin et sign up

--- components/Login.js
+++ components/Login.js
@@ -1,57 +1,16 @@
 import styles from '../styles/Login.module.css';
-import { useEffect, useState } from 'react';
-import { Modal, Button } from 'antd';
-import { login, logout } from '../reducers/user';
+import { useState } from 'react';
+import { Modal } from 'antd';
 import { useDispatch, useSelector } from 'react-redux';
+import SignUp from './SignUp'
+import SignIn from './SignIn'
 
 function Login() {
     const dispatch = useDispatch();
 	const user = useSelector((state) => state.user.value);
-
     const [isModalSignUpVisible, setIsModalSignUpVisible] = useState(false);
     const [isModalSigninVisible, setIsModalSignInVisible] = useState(false);
-    const [signUpFirstname, setSignUpFirstname] = useState('');
-    const [signUpUsername, setSignUpUsername] = useState('');
-	const [signUpPassword, setSignUpPassword] = useState('');
-	const [signInFirstname, setSignInFirstname] = useState('');
-    const [signInUsername, setSignInUsername] = useState('');
-	const [signInPassword, setSignInPassword] = useState('');
 
-	const handleConnection = () => {
-
-		fetch('http://localhost:3000/user/signin', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ firstname: signInFirstname, username: signInUsername, password: signInPassword }),
-		}).then(response => response.json())
-			.then(data => {
-				if (data.result) {
-					dispatch(login({ firstname: signInFirstname, username: signInUsername, token: data.token }));
-                    setSignInFirstname('');
-					setSignInUsername('');
-					setSignInPassword('');
-                    setIsModalSignInVisible(false);
-				}
-                console.log(login())
-			});
-	};
-
-    const handleRegister = () => {
-		fetch('http://localhost:3000/user/signup', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ username: signUpUsername, password: signUpPassword }),
-		}).then(response => response.json())
-			.then(data => {
-				if (data.result) {
-					dispatch(login({ username: signUpUsername, token: data.token }));
-                    setSignUpFirstname('');
-					setSignUpUsername('');
-					setSignUpPassword('');
-                    setIsModalSignUpVisible(false);
-				}
-			});
-	};
 
 	const showModalSignup = () => {
 		setIsModalSignUpVisible(!isModalSignUpVisible);
@@ -64,25 +23,11 @@ function Login() {
 	};
 
     const modalSignUpContent = (
-        <div>
-        <img src='logo.png' />
-        <p>Create  your hackatweet account</p>
-        <input type="text" placeholder="firstname" className={styles.signUpFirstname} onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname} />
-        <input type="text" placeholder="Username" className={styles.signUpUsername} onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} />
-        <input type="password" placeholder="Password" className={styles.signUpfirstname} onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} />
-        <button className={styles.signup} onClick={() => handleRegister()}>Sign up</button>
-        </div>
+       < SignUp/>
         );
 
     const modalSignInContent = (
-        <div>
-        <img src='logo.png' />
-        <p>Connect to your hackatweet account</p>
-        <input type="text" placeholder="firstname" className={styles.signInFirstname} onChange={(e) => setSignInFirstname(e.target.value)} value={signInFirstname} />
-        <input type="text" placeholder="Username" className={styles.signInUsername} onChange={(e) => setSignInUsername(e.target.value)} value={signInUsername} />
-        <input type="password" placeholder="Password" className={styles.signInPassword} onChange={(e) => setSignInPassword(e.target.value)} value={signInPassword} />
-        <button className={styles.signin} onClick={() => handleConnection()}>Sign in</button>
-        </div>
+      < SignIn/>
         );
 
 
---
+++ components/SignUp.js
@@ -0,0 +1,44 @@
+import styles from '../styles/SignUp.module.css';
+import { useState } from 'react';
+import { login } from '../reducers/user';
+import { useDispatch, useSelector } from 'react-redux';
+
+function SignUp() {
+
+    const dispatch = useDispatch();
+    const user = useSelector((state) => state.user.value);
+    const [signUpFirstname, setSignUpFirstname] = useState('');
+    const [signUpUsername, setSignUpUsername] = useState('');
+	const [signUpPassword, setSignUpPassword] = useState('');
+
+    const handleRegister = () => {
+		fetch('http://localhost:3000/user/signup', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/json' },
+			body: JSON.stringify({ firstname: signUpFirstname, username: signUpUsername, password: signUpPassword }),
+		}).then(response => response.json())
+			.then(data => {
+				if (data.result) {
+					dispatch(login({ firstname: signUpFirstname, username: signUpUsername, token: data.token }));
+                    //console.log(login({ firstname: signUpFirstname, username: signUpUsername, token: data.token }))
+                    setSignUpFirstname('');
+					setSignUpUsername('');
+					setSignUpPassword('');
+				}
+                console.log(login())
+			});
+	};
+
+return(
+        <div>
+        <img src='logo.png' />
+        <p>Create  your hackatweet account</p>
+        <input type="text" placeholder="firstname" className={styles.signUpFirstname} onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname} />
+        <input type="text" placeholder="Username" className={styles.signUpUsername} onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} />
+        <input type="password" placeholder="Password" className={styles.signUpfirstname} onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} />
+        <button className={styles.signup} onClick={() => handleRegister()}>Sign up</button>
+        </div>
+);
+}
+
+export default SignUp;
