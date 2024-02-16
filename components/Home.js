50e1d62f09f0aa51646629bdcc42fa3ff3114178
Axel Moras <axel.moras78@gmail.com>
2024-02-16 10:16:19 +0100

    jour2

--- components/Home.js
+++ components/Home.js
@@ -4,20 +4,31 @@ function Home() {
   
   return (
     <div className={styles.main}>
+
       <div className={styles.leftcontent}>
-        <img src='logo.png' className='logo'></img>
+        <img src='nouveaulogo.png' className='logo' height={200} width={150}></img>
+{/*         <p className={styles.infouser}>{Username}</p>  
+            <img pdp></img>
+            <p> Nom </p>
+*/}
         <button className={styles.logout}>Logout</button>
       </div>
+
       <div className={styles.home}>
         <h1 className={styles.title}>HOME</h1>
         <input type='text' placeholder="what's up ?" className={styles.whatsup}></input>
         <button className={styles.tweet}>Tweet</button>
       </div>
+
       <div className={styles.trends}>
         <h1 className={styles.titretrend}>Trends</h1>
-        <p className={styles.contentTrends}> LEAK SEXTAPE DRAKE </p>
+        <p className={styles.contentTrends}></p>
       </div>
-      <div className={styles.tweets}></div>
+
+      <div className={styles.tweets}>
+
+      </div>
+
     </div>
   );
 }
